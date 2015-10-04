10fdeb36406d6a553224028ed9a23e89803a2800
コンビニさん <mamaokawari@gmail.com>
2015-10-04 13:58:59 +0900

    Ver0.01 ナビゲーション作成・アイコンフォント対応（FontAwesome）から

---
+++ webimers.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html" charset="UTF-8">
+<meta http-equiv="content-style-type" content="text/css">
+<meta name="author" content="IMERS CO.">
+<meta name="description" content="IMERSのWeb版です。">
+<meta name="keywords" content="IMERS,Web,チャット,Chat,日本,Japenese">
+<link rel="stylesheet" href="styles/navi.css">
+<link rel="stylesheet" href="iconfont/css/font-awesome.min.css">
+
+<title>IMERS on Web</title>
+</head>
+<body>
+<!-- ナビゲーションバー設定　項目追加の際アイコンフォント忘れずに -->
+<div id="navi">
+<a href="index.html"><i class="fa fa-home"></i> TOP</a> | 
+<a href="about.html"><i class="fa fa-file-o"></i> ABOUT</a> | 
+<a href="login.php"><i class="fa fa-user"></i> Web版ログイン</a> | 
+<a href="register.php"><i class="fa fa-user-plus"></i> 新規登録</a>
+</div>
+</body>
