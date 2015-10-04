5efadca6b3bcc631f1264b6a2030be930d1b85e0
コンビニさん <mamaokawari@gmail.com>
2015-10-04 14:47:19 +0900

    Ver0.03 Styleにメインメニュー追加

--- webimers.php
+++ webimers.php
@@ -1,22 +1,35 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-<meta http-equiv="content-type" content="text/html" charset="UTF-8">
-<meta http-equiv="content-style-type" content="text/css">
-<meta name="author" content="IMERS CO.">
-<meta name="description" content="IMERSのWeb版です。">
-<meta name="keywords" content="IMERS,Web,チャット,Chat,日本,Japenese">
-<link rel="stylesheet" href="styles/navi.css">
-<link rel="stylesheet" href="iconfont/css/font-awesome.min.css">
-
-<title>IMERS on Web</title>
+	<head>
+		<meta http-equiv="content-type" content="text/html" charset="UTF-8">
+		<meta http-equiv="content-style-type" content="text/css">
+		<meta name="author" content="IMERS CO.">
+		<meta name="description" content="IMERSのWeb版です。">
+		<meta name="keywords" content="IMERS,Web,チャット,Chat,日本,Japenese">
+		<link rel="stylesheet" href="styles/navi.css">
+		<link rel="stylesheet" href="iconfont/css/font-awesome.min.css">
+		<title>IMERS on Web</title>
 </head>
 <body>
 <!-- ナビゲーションバー設定　項目追加の際アイコンフォント忘れずに -->
-<div id="navi">
-<a href="index.html"><i class="fa fa-home"></i> TOP</a> | 
-<a href="about.html"><i class="fa fa-file-o"></i> ABOUT</a> | 
-<a href="login.php"><i class="fa fa-user"></i> Web版ログイン</a> | 
-<a href="register.php"><i class="fa fa-user-plus"></i> 新規登録</a>
-</div>
+	<div id="navi">
+		<a href="index.html"><i class="fa fa-home"></i> TOP</a> | 
+		<a href="about.html"><i class="fa fa-file-o"></i> ABOUT</a> | 
+		<a href="login.php"><i class="fa fa-user"></i> Web版ログイン</a> | 
+		<a href="register.php"><i class="fa fa-user-plus"></i> 新規登録</a>
+	</div>
+	<div id="box-menu-home">
+		<h4>ホーム</h4>
+	</div><br>
+	<div id="box-menu-talk">
+		<h4>トーク</h4>
+	</div>
+	<div id="box-menu-friend">
+		<h4>友達</h4>
+	</div>
+	<div id="box-menu-trend">
+		<h4>トレンド</h4>
+	</div>
+	
+
 </body>
